f79b1cdfdd82e21f301427f23b7a55981f829ada
Foomie <oh.no.fumi23@gmail.com>
2017-10-24 00:56:29 +0900

    20171023FoodNoteアプリ作成と更新。

---
+++ foodnote-app/src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { InputComponent } from './input/input.component';
+import { MenuComponent } from './menu/menu.component';
+
+//インポートする
+import { RouterModule } from '@angular/router';
+
+import { CalenderComponent } from './calender/calender.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+
+//パスと対応するコンポーネントを書く
+export const AppRoutes = [
+  {path: "", component: MenuComponent},
+  {path: "input", component: InputComponent},
+  {path: "calender", component: CalenderComponent},
+  {path: "list", component: ListComponent},
+  {path: "detail", component: DetailComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CalenderComponent,
+    ListComponent,
+    DetailComponent,
+    InputComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
